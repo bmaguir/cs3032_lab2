4fa32ad72ef4c9aeb15ef2956bc47e30231b29ee
Brian Maguire <bmaguir@tcd.ie>
2014-10-08 20:33:35 +0100

    Thread pooling, exit() function and joining

--- lab2_client.pl
+++ lab2_client.pl
@@ -1,17 +1,27 @@
 use IO::Socket;
 
-my $socket = IO::Socket::INET->new(
-    PeerAddr =>'localhost',
-	PeerPort => '2000',
-    Proto => 'tcp'
-) or die("Error :: $!");
+while(true){
 
-print "please enter the message you wish to send \n";
-#takes a line from the keyboard 
-my $msg=<STDIN>;
+	print "please enter the message you wish to send \n";
+	#takes a line from the keyboard 
+	$msg=<STDIN>;
+	if($msg eq "end\n"){
+		exit(0);
+	}
+	
+	my $socket = IO::Socket::INET->new(
+		PeerAddr =>'localhost',
+		PeerPort => '2000',
+		Proto => 'tcp'
+	) or die("Error :: $!");
 
-print ($socket $msg);
+	print ($socket $msg);
+	shutdown($socket, 1);
+	my $recv_line;
+	#prints out what is received from the server
+	while ($recv_line = <$socket>){
+		print "$recv_line\n";
+	}
 
-
-
-close $socket;
+	close $socket;
+}
--- lab2_server.pl
+++ lab2_server.pl
@@ -1,21 +1,32 @@
  use Thread;
  use IO::Socket;
  {
+ 
+	my $port = '2000';
 	$socket = IO::Socket::INET->new(
 		LocalHost => 'localhost',
-		LocalPort => '2000',
+		LocalPort => $port,
 		Proto => 'tcp',
 		Listen => 1,
 		Reuse => 1
 	)
 	or die "Coudn't open socket$!" unless $socket;
-	print "socket listening on port 2000\n";
+	print "socket listening on port $port\n";
 	my @thr;
 
 	while(true){
 		my $new_sock = $socket->accept();
 		#adds a new thread to the pool to handle the client 
-		push(@thr, new Thread \&sub1, $new_sock);
+		$thread_count = threads->list();
+		#if the thread pool has not been used, start new thread
+		if(thread_count < 5){
+			push(@thr, new Thread \&sub1, $new_sock);
+		}
+		foreach(@thr){
+			if ($_->is_joinable()) {
+				$_->join();
+			}
+		}
 	}
 
 
@@ -27,10 +38,22 @@
 		}
 		if($msg eq "KILL_SERVICE\n"){
 			print $msg;
+			close(thread_socket);
 			exit &end;
 		}
 		else{
-			print "client said $msg";
+			if($msg eq "HELO text\n")
+			{
+				print "client said $msg";
+				print($thread_socket "HELO text\nIP:[ip address]\nPort: $port\nStudentID: 10366921");
+				close($thread_socket);
+			}
+			else
+			{
+				print "client said $msg";
+				print($thread_socket "ack");
+				close($thread_socket);
+			}
 		}
 	}
 
